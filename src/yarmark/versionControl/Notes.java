2f8b1f5ac922d68347823079c2faf3a7110e8948
unknown <student1@FB-W213-W8.academic.touro.edu>
2015-09-10 20:01:16 -0400

    Added version control package

---
+++ src/yarmark/versionControl/Notes.java
@@ -0,0 +1,48 @@
+package yarmark.versionControl;
+
+public class Notes {
+/*
+ * version control- a server to save code
+ * we will use Git to save code and upload homework.
+ * Git- protocol and program name
+ * GitHub- online server
+ * Git is a console, without a GUI
+ * 
+ * source tree is a version control program with GUI- can use this at home
+ * 
+ * command prompt
+ * in windows- Command Prompt
+ * in mac- terminal
+ * 
+ * C: drive name
+ * Users: name of directory off root drive
+ * student1: name of directory in Users
+ * directories are deliminated by '\'
+ * to change drives, need to use capital letter
+ * dir lists all the folders in the currect location
+ * 
+ * macs
+ * do not have directories
+ * instead of dir, use ls
+ * 
+ * in cmd:
+ * cd to project folder
+ * git
+ * git init
+ * git status
+ * git add .classpath, .project, src/
+ * make a wordpad txt file called gitignore
+ * type bin/ and settings/ in the file
+ * git add .gitignore
+ * git commit -m "Added Projectile and QuadraticEquation and PythagoreanTheorem"
+ * -m is the title "THIS IS THE COMMIT MESSAGE"
+ * you call look back and the the commit history and know what you did
+ * git keeps a history with who, time, and message of change
+ * if git asks who you are, put your name/email
+ * git gives the two command options for both choices
+ * 
+ * MY WORKSPACE IS: E:\MCO 152 Computer Methodology
+ * MY PROJECT IS: E:\MCO 152 Computer Methodology\programs
+ * 
+ */
+}
