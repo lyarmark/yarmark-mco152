b85a5c0178dc68c290b36ba3cca6e925d390472d
Leah Yarmark <lyarmark@student.touro.edu>
2015-10-28 00:11:54 -0400

    projectile time frame

--- src/yarmark/physics/ProjectileComponent.java
+++ src/yarmark/physics/ProjectileComponent.java
@@ -7,15 +7,22 @@ import java.awt.geom.Line2D;
 import javax.swing.JComponent;
 
 public class ProjectileComponent extends JComponent {
+	private int angle = 31;
+	private int velocity = 20;
+	private int time = 0;
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
-		Projectile p = new Projectile(31, 20, 0);
+		Projectile p = new Projectile(angle, velocity, time);
 		double x1 = p.getX();
 		double y1 = p.getY();
 
-		for (int i = 0; p.getY() >= 0; i++) {
+		// to show the full arc
+		// for (int i = 0; p.getY() >= 0; i++) {
+
+		// to show the first 20 seconds of the arc
+		for (int i = 0; i <= 20; i++) {
 			p.setTime(i + 1);
 			g2.draw(new Line2D.Double(x1, getHeight() - y1, p.getX(), getHeight() - p.getY()));
 			x1 = p.getX();
